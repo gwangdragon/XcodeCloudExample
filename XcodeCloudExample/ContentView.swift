a19c4bdef389f3e2638fbf7ae800c2859bac6b65
RON <ron@alltaxmobile.co.kr>
2024-12-16 23:41:11 +0900

    Initial Commit

---
+++ XcodeCloudExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodeCloudExample
+//
+//  Created by gwangyonglee on 12/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Xcode Cloud Example")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeCloudExample/XcodeCloudExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudExampleApp.swift
+//  XcodeCloudExample
+//
+//  Created by gwangyonglee on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
